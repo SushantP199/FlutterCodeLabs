2e0ca199719b06824852348108665efffec4e4cd
SushantP199 <sushantpagam199@gmail.com>
2022-06-12 00:51:05 +0530

    completed mount details page just using core widgets of material library

--- mount/lib/main.dart
+++ mount/lib/main.dart
@@ -87,7 +87,7 @@ final List<CategoryModel> categories = [
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: SplashPage(),
+    home: DetailsPage(), //SplashPage(),
   ));
 }
 
@@ -165,6 +165,130 @@ class MountsApp extends StatelessWidget {
   }
 }
 
+class DetailsPage extends StatelessWidget {
+
+  @override 
+  Widget build (BuildContext context){
+
+    var selectedItem = mountItems[0];
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children:  [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(40), bottomRight: Radius.circular(40)),
+              child: Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(30),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(selectedItem.path),
+                        fit : BoxFit.cover
+                      )
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+                          begin: Alignment.center,
+                          end: Alignment.bottomCenter
+                        )
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 30, left: 30, right: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          selectedItem.name,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          selectedItem.location,
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  AppBar(
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    iconTheme: const IconThemeData(color: Colors.white),
+                    centerTitle: true,
+                    title: const Icon(
+                      Icons.terrain,
+                      size: 40,
+                    ),
+                    actions: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        child: const Icon(
+                          Icons.pending,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                DetailsRatingBar(),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20), 
+                        child: Text (
+                         'About ${selectedItem.name}',
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                        child: Text (
+                          selectedItem.description,
+                          style: const TextStyle(
+                            fontSize: 12,
+                          ),
+                       ),
+                      )
+                    ],
+                  ),
+                ),
+                DetailsBottomAction()
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
+}
 
 
 /* Widgets */
@@ -491,6 +615,106 @@ class AppBottomBarState extends State<AppBottomBar> {
           }
         )
       ),
+    );
+  }
+}
+
+class DetailsRatingBar extends StatelessWidget {
+
+  final sampleRatingData = {
+    'Rating': '4.6',
+    'Price': '\$12',
+    'Open': '24hrs'
+  };
+
+  @override 
+  Widget build (BuildContext context){
+
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: List.generate(
+          sampleRatingData.entries.length, 
+          (index) => Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.withOpacity(0.2), width: 2),
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              children: [
+                Text(
+                  sampleRatingData.entries.elementAt(index).key,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  sampleRatingData.entries.elementAt(index).value,
+                  style: const TextStyle(
+                    color: mainColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                  ),
+                )
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsBottomAction extends StatelessWidget {
+
+  @override
+  Widget build (BuildContext context){
+
+    return  Padding(
+      padding:const  EdgeInsets.only(left: 20, right: 20, bottom: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: Material(
+              borderRadius: BorderRadius.circular(15),
+              color: mainColor,
+              child: InkWell(
+                highlightColor: Colors.white.withOpacity(0.2),
+                splashColor: Colors.white.withOpacity(0.2),
+                onTap: (){},
+                child: Container(
+                  padding: const EdgeInsets.all(21),
+                  child: const Text(
+                    'Book Now',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.white,
+              border: Border.all(color: mainColor, width: 2)
+            ),
+            child: const Icon(
+              Icons.turned_in_not,
+              color: mainColor,
+              size: 25,
+            ),
+          )
+        ],
+      )
     );
   }
 }
